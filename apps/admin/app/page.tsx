e3cbe05a3e2012e63d57e4278d1b7df05bd4039a
ldh <996970752@qq.com>
2026-05-14 00:42:06 +0800

    fix: 修复vercel构建失败的bug

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from "next/image";
-import { prisma } from '@help-platform/database';
-
-  const count = await prisma.user.count();
-  console.log(count);
 
 export default function Home() {
-    console.log(count);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
